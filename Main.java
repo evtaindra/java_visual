60378858612646929af01743fe689ffc0f0c7005
evtaindra <evtapribadi@gmail.com>
2025-10-29 12:52:01 +0700

    Refactor Main class to extend Application and display character information in a JavaFX window

--- Main.java
+++ Main.java
@@ -1,11 +1,33 @@
-public class Main{
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {       
+        Character Evta = new Character("Evta Indra", 100, 1);
+        Label nameLabel = new Label("Name: " + Evta.showName());
+
+        StackPane root = new StackPane();
+        root.getChildren().add(nameLabel);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("Character Info");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello Evta");
 
-        Character Evta = new Character("Evta Indra",100,1);
+        //untuk menjalankan aplikasi, buka terminal dan ketikan perintah berikut
+        // javac --module-path "D:\perkuliahan2025\Java_visual\javafx-sdk-21.0.9\lib" --add-modules javafx.controls Main.java
 
-        System.out.println("Name : " + Evta.showName());
-
+        //setelah tidak ada Error lalu jalankan perintah berikut
+        //java --module-path "D:\perkuliahan2025\Java_visual\javafx-sdk-21.0.9\lib" --add-modules javafx.controls Main
+        launch(args);
     }
      
 }
